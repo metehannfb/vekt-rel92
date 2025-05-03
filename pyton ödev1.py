ed3002e5de9bfd54bc96ddc2d876fa42e37d8085
metehannfb <metehanbolukbas06@gmail.com>
2025-05-03 14:24:35 +0300

    pytonödev.1

---
+++ pyton ödev1.py
@@ -0,0 +1,21 @@
+input
+print("Merhaba")
+print("metehan bölükbas")
+
+
+print("metehan bölükbas","\npyton92","\n338")
+
+
+
+print("İstiklâlMarşı","\nKorkma,sönmez bu şafaklarda yüzen al sancak:","\nSönmeden yurdumun üstünde tüten en son ocak.",",\nO benim milletimin yıldızıdır,parlayacak;","\nO benimdir,o benim milletimindir ancak")
+
+
+print("******Öğrenci bilgisi******")
+print("Adı  :Metehan Bölükbas     ")
+print("Soyadı:Bölükbas            ")
+print("Numarası:338               ")  
+print("***************************")
+print("Ali'nin annesi""'gel'""dedi")
+print("Metehan " * 100)
+print("\n".join(["Metehan"] * 10))
+
