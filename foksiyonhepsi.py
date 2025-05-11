507c8a392810b8fa8699c23feac3ff8d4a0aa7cd
metehannfb <metehanbolukbas06@gmail.com>
2025-05-11 16:08:07 +0300

    foksiyonhepsi

---
+++ foksiyonhepsi.py
@@ -0,0 +1,22 @@
+
+def selamlar():
+    print("Selamlar")
+    print("İyi günler")
+# selamlar()
+def merhabade():
+    print("Merhaba")
+    print("Nailsin1")
+selamlar()
+merhabade()
+print("=========================================================")
+# değer alan foksiyonlar
+def yaz(yazilcakşey):
+    print(yazilcakşey)
+
+    yaz("Selam dost ")
+def topla(a,b):
+    print("Topla", a*b)
+
+topla(60,10)
+
+
